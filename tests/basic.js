7d9d001ef6de61e4f22ccf437713779654c4416f
hardfist <yangjianzju@gmail.com>
2025-02-24 15:48:55 +0800

    chore: add tsfn test

--- tests/basic.js
+++ tests/basic.js
@@ -1,9 +1,21 @@
 import { tsfn } from '../index.js';
 //import whyIsNodeRunning from 'why-is-node-running';
+const registry = new FinalizationRegistry((val) => {
+    console.log('finalize', val);
+})
+function main() {
+    function callback(err,num){
+        console.log('num:',num);
+    }
+    const counter = new tsfn.Counter(callback);
+    const counter2 = new tsfn.Counter(callback);
+    counter.add(10);
+    counter.add(5);
+    registry.register(counter, 'counter');
+    registry.register(callback,'callback');
+    registry.register(counter2,'counter2');
 
-const counter = new tsfn.Counter((err, num) => {
-    console.log(err, num)
-});
-counter.add(10);
-counter.add(5);
+}
+
+main();
 //setImmediate(() => whyIsNodeRunning())
