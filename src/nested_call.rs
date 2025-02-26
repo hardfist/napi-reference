6e099dde9ca2d80db40111d484f6229e9fab4624
hardfist <yangjianzju@gmail.com>
2025-02-26 21:24:32 +0800

    chore: add tsfn

--- src/nested_call.rs
+++ src/nested_call.rs
@@ -3,19 +3,25 @@ use napi_derive::napi;
 #[napi]
 pub mod nested_call {
 
-    use napi::bindgen_prelude::*;
+    use std::sync::Arc;
+
+    use napi::{bindgen_prelude::*, threadsafe_function::ThreadsafeFunction};
 
 
     #[napi(custom_finalize)]
     pub struct Compiler {
-        asset_path_callback: FunctionRef<i32,i32>
+        asset_path_callback: FunctionRef<i32,i32>,
+        tsfn:Arc<ThreadsafeFunction<i32, i32, i32, false>>
     }
     #[napi]
     impl Compiler {
         #[napi(constructor)]
-        pub fn new(callback: Function<i32,i32>) -> Self {
+        pub fn new(env: Env,callback: Function<i32,i32>) -> Self {
+            let mut tsfn: napi::threadsafe_function::ThreadsafeFunction<i32, i32, i32, false> = callback.build_threadsafe_function().build().unwrap();
+            tsfn.unref(&env).unwrap();
             Compiler {
-                asset_path_callback:callback.create_ref().unwrap()
+                asset_path_callback:callback.create_ref().unwrap(),
+                tsfn: Arc::new(tsfn)
             }
         }
         #[napi]
@@ -23,7 +29,9 @@ pub mod nested_call {
             self.call_assset_path_hook(env,n)
         }
         #[napi]
-        pub fn call_assset_path_hook(&self,env: Env,n:i32) -> i32{
+        pub fn call_assset_path_hook(&self,env: Env,n:i32) -> i32{  
+             // this will cause dead lock
+             // let result = block_on(self.tsfn.call_async(100)).unwrap(); 
              let result = self.asset_path_callback.borrow_back(&env).unwrap().call(n).unwrap();
              result
         }
